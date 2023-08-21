36bd2109020bac3ca118c42683689f5913a51f2d
Seungwon Park <psw70408@gmail.com>
2023-08-21 14:24:07 +0900

    post model 추가

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hit = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def update_counter(self):
+        self.hit += 1
+        self.save()
+        return self.hit
