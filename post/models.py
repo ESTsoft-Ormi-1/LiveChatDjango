e49e1a143c034313c3ab14753f41146ebcac2f6d
Seungwon Park <psw70408@gmail.com>
2023-08-22 14:41:06 +0900

    tag

--- post/models.py
+++ post/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Tag(models.Model):
-    # id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, unique=True, primary_key=True)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
