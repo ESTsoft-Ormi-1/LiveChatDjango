c55c13cf9536c60326412ca2d81b08f93f820d0b
sungbinlee <sunglee@addu.edu.ph>
2023-08-24 16:40:22 +0900

    feat: GET 요청시 채팅방 주소값 반환

--- chat/models.py
+++ chat/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
+
+    @property
+    def chat_group_name(self):
+        return self.make_chat_group_name(room=self)
+    
+    @staticmethod
+    def make_chat_group_name(room=None, room_pk=None):
+        return "chat-%s" % (room_pk or room.pk)
