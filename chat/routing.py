5e64376cddd98fed2196e7f95448b3c698ce34e7
sungbinlee <sunglee@addu.edu.ph>
2023-08-24 16:39:40 +0900

    refactor: user_name 기반을 room_pk 로 변경

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from chat import consumers
 
 websocket_urlpatterns = [
-    path("ws/chat/<str:room_name>/chat/", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<str:room_pk>/chat/", consumers.ChatConsumer.as_asgi()),
 ]
--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from chat import views
+from django.urls import re_path, include
+from rest_framework import routers
+from chat.views import RoomViewSet
+
+router = routers.DefaultRouter()
+router.register('room', RoomViewSet)
+
 
 urlpatterns = [
-    
+    re_path(r'^', include(router.urls)),
 ]
