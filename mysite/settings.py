03b6947208e98337be323b35d798f4d165c394d6
sungbinlee <sunglee@addu.edu.ph>
2023-08-24 09:48:45 +0900

    feat: REST 프레임워크 권한 구성 업데이트

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,15 @@ INSTALLED_APPS = [
     'chat',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
