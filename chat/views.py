9a661809469a717dbae992d5e0390d67d9e00648
sungbinlee <sunglee@addu.edu.ph>
2023-08-24 16:41:21 +0900

    feat: chat viewsets 구현

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
