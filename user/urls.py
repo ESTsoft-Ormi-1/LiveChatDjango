c229b0e5bae02eedc2484eb4288c61c2180f935b
simminjeong <zngkgk97@gmail.com>
2023-08-24 14:32:25 +0900

    test: jwt인증 test코드 추가

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from .views import TestJWTAuth
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # JWT 토큰 생성 URL 추가
+    path('test-jwt-auth/', TestJWTAuth.as_view(), name='test_jwt_auth'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+
+class TestJWTAuth(APIView):
+    permission_classes = [IsAuthenticated]  # 이 view를 호출하기 위해서는 인증이 필요하다는 뜻 입니다(DRF 자체에서 아무나 들어올 수 없도록 막아주는 기본 기능입니다.)
+
+    def get(self, request):
+        user = request.user  # 현재 인증된 사용자 정보 가져오기
+        return Response({"message": f"안녕하세요, {user}님! JWT인증이 완료되었습니다!."})
