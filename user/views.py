d306fb1aba1f136ef4c4e85fe434ba69bdb3c7fd
simminjeong <zngkgk97@gmail.com>
2023-08-28 18:29:25 +0900

    test: 회원가입(구현중), 로그인(완료) 커스터마이징

--- user/views.py
+++ user/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserProfileSerializer
-from .models import UserProfile
+from .models import UserProfile, User
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView
+from rest_framework.permissions import AllowAny
+
+
+class CustomRegisterView(RegisterView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_201_CREATED:
+            user_email = response.data['email']
+            response.data['message'] = f"{user_email}님 회원가입이 완료되었습니다."
+
+            # 회원가입 후 프로필 생성
+            user = User.objects.get(email=user_email)  # 이 부분을 추가해 user 객체를 가져옴
+            UserProfile.objects.create(user=user)
+            user_profile = UserProfile.objects.get(user=user)
+            serializer = UserProfileSerializer(user_profile)
+            response.data['profile'] = serializer.data
+
+        return response
+
+class CustomLoginView(LoginView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            user_email = self.user.email
+            response.data['message'] = f"{user_email}님 환영합니다."
+
+            user_profile = UserProfile.objects.get(user=self.user)
+            serializer = UserProfileSerializer(user_profile)
+            response.data['profile'] = serializer.data
+
+        return response
+
 
 class TestJWTAuth(APIView):
     permission_classes = [IsAuthenticated]  # 이 view를 호출하기 위해서는 인증이 필요하다는 뜻 입니다(DRF 자체에서 아무나 들어올 수 없도록 막아주는 기본 기능입니다.)
