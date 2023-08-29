c9d843c1a721fd9aa733a543e23435dd405f5056
sungbinlee <sunglee@addu.edu.ph>
2023-08-29 14:47:54 +0900

    feat: message createview 작성

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
-from .models import Room
-from .serializers import RoomSerializer
+from rest_framework import viewsets, generics
+from .models import Room, Chat
+from .serializers import RoomSerializer, ChatSerializer
+
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class ChatCreateView(generics.CreateAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
