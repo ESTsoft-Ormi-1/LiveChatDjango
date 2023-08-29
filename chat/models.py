d669f52babc92b4ab003d9fa53b1085e94181413
sungbinlee <sunglee@addu.edu.ph>
2023-08-29 14:33:16 +0900

    feat: Room owner, created_at and updated_at added

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def chat_group_name(self):
