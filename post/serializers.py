8e993204f87672622ba2cebfca37b2d9ec160702
Seungwon Park <psw70408@gmail.com>
2023-08-31 17:57:02 +0900

    글쓴이 설정

--- post/serializers.py
+++ post/serializers.py
@@ -15,10 +15,14 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # exclude = ['writer']
     
-    def create(self, validated_data):
-        # 현재 로그인한 사용자를 작성자로 설정
+    # def create(self, validated_data):
+    #     # 현재 로그인한 사용자를 작성자로 설정
+    #     validated_data['writer'] = self.context['request'].user
+    #     return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
         validated_data['writer'] = self.context['request'].user
-        return super().create(validated_data)
+        return super().update(instance, validated_data)
 
 class WriterSerializer(serializers.ModelSerializer):
     class Meta:
