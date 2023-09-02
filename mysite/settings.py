47cb3243ffd4625700536e64b02242190e9dce7a
sungbinlee <sunglee@addu.edu.ph>
2023-09-02 10:19:59 +0900

    refacotr: 토큰인증시간 연장

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    #'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=365),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60), 
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
 }
 
