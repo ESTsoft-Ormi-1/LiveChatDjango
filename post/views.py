09ab0fcb8f214c87ddff524069f35c7e37125054
sungbinlee <sunglee@addu.edu.ph>
2023-09-02 21:31:17 +0900

    feat: post 유저 정보 반환

--- post/views.py
+++ post/views.py
@@ -6,8 +6,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.throttling import UserRateThrottle
 from .models import Post, Tag, Category
 from .forms import PostForm
-
-from .serializers import PostSerializer, TagSerializer, CategorySerializer
+from chat.models import Room
+from chat.serializers import RoomSerializer
+from .serializers import PostSerializer, TagSerializer, CategorySerializer, WriterSerializer
 from django.db.models import Q
 
 
@@ -19,20 +20,21 @@ class Index(APIView):
         posts = Post.objects.all()
         serialized_posts = PostSerializer(posts, many=True)
         return Response(serialized_posts.data)
-    
 
 
 class DetailView(APIView):
     # 인증된 사용자만 접근 가능
     permission_classes = [IsAuthenticated] 
     # 사용자 요청 속도 제한 설정
-    throttle_classes = [UserRateThrottle]
+    # throttle_classes = [UserRateThrottle]
 
     def get(self, request, pk):
         try:
             post = Post.objects.get(pk=pk)
         except Post.DoesNotExist:
             return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        room, created = Room.objects.get_or_create(post=post)
 
         # 조회수 증가
         post.hit += 1
@@ -50,7 +52,9 @@ class DetailView(APIView):
             "content": serialized_post['content'], 
             "writer": serialized_writer,
             "tags": serialized_tags,
-            "hit": post.hit
+            "hit": post.hit,
+            "room_data": RoomSerializer(room, context={'request': request}).data
+
         }
         
         return Response(data)
@@ -66,6 +70,10 @@ class Write(APIView):
         serializer = PostSerializer(data=request.data)
         
         if serializer.is_valid():
+            post_data = serializer.validated_data
+            room_owner = request.user
+            room_title = post_data.get('title')  # Use the post title as the room title
+            room = Room.objects.create(owner=room_owner, name=room_title)
             # hashtags
             tags_data = serializer.validated_data.pop('tags', [])
             tags_list = []
@@ -75,7 +83,7 @@ class Write(APIView):
                     tag, created = Tag.objects.get_or_create(name=tag_name)
                     tags_list.append(tag)
 
-            post = serializer.save(writer=request.user)
+            post = Post.objects.create(writer=request.user, room=room, **post_data)
             post.tags.set(tags_list)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
